9ebe4049cfbd59f45fd24b151e0b0be3496feae8
pablo.gallardo <pablo.gallardo@teamdetroit.com>
2015-02-11 14:57:51 -0300

    New MerkelHash

--- src/main.cpp
+++ src/main.cpp
@@ -2806,7 +2806,7 @@ bool InitBlockIndex() {
         printf("%s\n", hash.ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
+        assert(block.hashMerkleRoot == uint256("0xea27d6425bccba0b2f5c2642da339d34de3af594f0c1a8919ec3acbb2f62b1b4"));
         block.print();
         assert(hash == hashGenesisBlock);
 
