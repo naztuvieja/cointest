fc226cd745e17fe7156970fbf5e15202fe9d8c5d
pablo.gallardo <pablo.gallardo@teamdetroit.com>
2015-02-11 11:29:02 -0300

    Name replacement

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("CTS");
+    case mBTC: return QString("mCTS");
+    case uBTC: return QString::fromUtf8("μCTS");
     default: return QString("???");
     }
 }
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Cointests");
+    case mBTC: return QString("Milli-Cointests (1 / 1,000)");
+    case uBTC: return QString("Micro-Cointests (1 / 1,000,000)");
     default: return QString("???");
     }
 }
